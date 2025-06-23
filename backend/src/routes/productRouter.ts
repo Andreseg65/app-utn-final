9047c906c46574717a2424e9a93707dd9a61e7f6
Andres <andreseg65@gmail.com>
2025-06-23 15:15:54 -0300

    Agregando ruta y controlador de búsqueda de productos por nombre

--- backend/src/routes/productRouter.ts
+++ backend/src/routes/productRouter.ts
@@ -1,12 +1,20 @@
-import { Router } from "express"
-import { createProduct, deleteProduct, getAllProducts, updateProduct } from "../controllers/productControllers"
-import { authMiddleware } from "../middleware/authMiddleware"
+import { Router } from "express";
+import {
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  updateProduct,
+  searchProducts,
+} from "../controllers/productControllers";
+import { authMiddleware } from "../middleware/authMiddleware";
 
-const productRouter = Router()
+const productRouter = Router();
 
-productRouter.get("/", getAllProducts)
-productRouter.post("/", authMiddleware, createProduct)
-productRouter.delete("/:id", authMiddleware, deleteProduct)
-productRouter.patch("/:id", authMiddleware, updateProduct)
+productRouter.get("/", getAllProducts);
+productRouter.post("/", authMiddleware, createProduct);
+productRouter.delete("/:id", authMiddleware, deleteProduct);
+productRouter.patch("/:id", authMiddleware, updateProduct);
+productRouter.get("/search", searchProducts);
 
-export { productRouter }
+// Export the productRouter
+export { productRouter };
